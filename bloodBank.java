1828eb09b2cab7dd1864dddead91301dfe945976
Thirumalai P <114209967+Thiru17398@users.noreply.github.com>
2023-01-06 20:29:48 +0530

    Added comments

--- bloodBank.java
+++ bloodBank.java
@@ -12,8 +12,8 @@ public class bloodBank {
 		Statement st=con.createStatement();
 		System.out.println("1.Donor Registration"+"\n"+"2.Search for Donor"+"\n"+"3.Blood Availability");
 		System.out.print("Enter Your Choice : ");
-		int choice = sc.nextInt();
-		Donor donor = new Donor();
+		int choice = sc.nextInt(); // getting choice
+		Donor donor = new Donor(); // creating an object for class Donor 
 		switch(choice) {
 		case 1:
 			donor.getDetails();
