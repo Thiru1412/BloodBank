bc1d5b853ca6eaf934dedf113b803d0ae732a5b9
Thirumalai <thir21223.it@rmkec.ac.in>
2023-01-16 19:41:44 +0530

    Main Class Updated with threshold class

--- bloodBank.java
+++ bloodBank.java
@@ -7,13 +7,14 @@ public class bloodBank {
 	public static void main(String[] args) {
 	try {
 		Scanner sc = new Scanner(System.in);
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/test","root","");
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3307/bloodbank","root","Thirumalai_14");
 		Statement st=con.createStatement();
-		System.out.println("1.Donor Registration"+"\n"+"2.Search for Donor"+"\n"+"3.Blood Availability");
-		System.out.print("Enter Your Choice : ");
-		int choice = sc.nextInt(); // getting choice
-		Donor donor = new Donor(); // creating an object for class Donor 
+		System.out.println("1.Donor Registration"+"\n"+"2.Search for Donor"+"\n"+"3.Blood Availability"+"\n"+"4.Set Threshold for Blood Groups"+"\n"+"5.Show Threshold Configuration");
+		System.out.print("\nEnter Your Choice : ");
+		int choice = sc.nextInt();
+		Donor donor = new Donor();
+		Threshold threshold = new Threshold();
 		switch(choice) {
 		case 1:
 			donor.getDetails();
@@ -25,12 +26,17 @@ public class bloodBank {
 		case 3:
 			donor.displayAvailability();
 			break;
-		
+		case 4:
+			threshold.setThreshold();
+			break;
+		case 5:
+			threshold.showConfiguration();
+			break;
+			
 		}
 	}
 	catch(Exception e){
 		System.out.println("Exception : "+e);
 	}
 	}
-
 }
