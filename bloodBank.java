72150659f778317f9f8bbb9c3adad09e42326ffa
Thirumalai P <114209967+Thiru17398@users.noreply.github.com>
2023-07-05 22:26:51 +0530

    main class updated

--- bloodBank.java
+++ bloodBank.java
@@ -1,4 +1,4 @@
-package bloodbank;
+package BloodBank;
 import java.sql.*;
 import java.util.*;
 
@@ -10,11 +10,15 @@ public class bloodBank {
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3307/bloodbank","root","Thirumalai_14");
 		Statement st=con.createStatement();
-		System.out.println("1.Donor Registration"+"\n"+"2.Search for Donor"+"\n"+"3.Blood Availability"+"\n"+"4.Set Threshold for Blood Groups"+"\n"+"5.Show Threshold Configuration");
-		System.out.print("\nEnter Your Choice : ");
-		int choice = sc.nextInt();
 		Donor donor = new Donor();
 		Threshold threshold = new Threshold();
+		int choice=0;
+		while(choice!=6) {
+		System.out.println("                                 --------------------------------------------------------------------------                                 ");
+		System.out.println("1.Donor Registration"+"\n"+"2.Search for Donor"+"\n"+"3.Blood Availability"+"\n"+"4.Set Threshold for Blood Groups"+"\n"+"5.Show Threshold Configuration\n6.Exit\n");
+		System.out.print("Enter Your Choice : ");
+		choice = sc.nextInt();
+		System.out.println();
 		switch(choice) {
 		case 1:
 			donor.getDetails();
@@ -22,7 +26,7 @@ public class bloodBank {
 			break;
 		case 2:
 			donor.displayData();
-			break;
+			break; 
 		case 3:
 			donor.displayAvailability();
 			break;
@@ -32,11 +36,17 @@ public class bloodBank {
 		case 5:
 			threshold.showConfiguration();
 			break;
-			
+		case 6:
+			System.out.println("Succesfully Exited !!!");
+			continue;
+		default:
+			System.out.println("Enter Valid Choice!!!");
+			}
+		System.out.println();
 		}
 	}
 	catch(Exception e){
-		System.out.println("Exception : "+e);
+		System.out.println("Exception : "+e.getMessage());
 	}
 	}
 }
